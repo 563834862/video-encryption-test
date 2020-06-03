3b569cf9cdf1187ad5e49de55e6b7b943902ce49
yexh_3 <18396118119@163.com>
2020-06-03 11:52:10 +0800

    Add: 导出spring boot

---
+++ src/main/java/com/yexh/videoencryptiontest/VideoEncryptionTestApplication.java
@@ -0,0 +1,13 @@
+package com.yexh.videoencryptiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoEncryptionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoEncryptionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yexh/videoencryptiontest/VideoEncryptionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yexh.videoencryptiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoEncryptionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
